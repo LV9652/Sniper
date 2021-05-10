c70e08da65b3c5414dea8af833a180da1d8d2898
LV <jiahe.lyu@mail.utoronto.ca>
2021-05-09 20:39:13 -0400

    remove initial enemies

--- Assets/FPS/Scripts/EnemyCreator.cs
+++ Assets/FPS/Scripts/EnemyCreator.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class EnemyCreator : MonoBehaviour
 {
-    public GameObject enemyMake;
-    public double spawn_interval;
+    public GameObject enemy;
+    public float spawn_interval;
     public Vector3 spawn_position;
-    double nasru;
 
     // Start is called before the first frame update
     void Start()
     {
-        nasru = spawn_interval;
+        StartCoroutine(createEnemy());
+    }
+
+    public void CreateObject()
+    {
+        GameObject go = Instantiate(enemy);
+        go.transform.position = spawn_position;
+        go.SetActive(true);
+    }
+    IEnumerator createEnemy()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawn_interval);
+            CreateObject();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        nasru -= Time.deltaTime;
-        if (nasru <= 0)
-        {
-            nasru = spawn_interval;
-            GameObject go = GameObject.Instantiate(enemyMake);
-            // go.transform.position = transform.position;
-            go.transform.position = spawn_position;
-        }
     }
 }
